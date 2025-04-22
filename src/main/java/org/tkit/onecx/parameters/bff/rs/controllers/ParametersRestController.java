8c79a3a8de253e0d5fbad37baa5aa20433171578
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-04-22 11:33:07 +0200

    feat: aligned to internal api changes (#99)

--- src/main/java/org/tkit/onecx/parameters/bff/rs/controllers/ParametersRestController.java
+++ src/main/java/org/tkit/onecx/parameters/bff/rs/controllers/ParametersRestController.java
@@ -57,8 +57,8 @@ public class ParametersRestController implements ParametersApiService {
     }
 
     @Override
-    public Response getAllApplications() {
-        try (Response response = client.getAllApplications()) {
+    public Response getAllProducts() {
+        try (Response response = client.getAllProducts()) {
             return Response.status(response.getStatus()).entity(mapper.map(response.readEntity(Product[].class)))
                     .build();
         }
