38f2aa174133cbfd7aac141ee232b9a64f9371ec
Andrej Petras <andrej@lorislab.org>
2025-05-20 11:19:01 +0200

    feat: update parent to 1.1.0, update docker images

--- src/test/java/org/tkit/onecx/parameters/bff/rs/HistoryRestControllerIT.java
+++
@@ -1,8 +0,0 @@
-package org.tkit.onecx.parameters.bff.rs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HistoryRestControllerIT extends HistoryRestControllerTest {
-
-}
